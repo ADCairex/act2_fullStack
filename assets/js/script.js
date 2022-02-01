0a371ac69eaa4810efbbb8d533f0e7028bfe8bad
ADCairex <angelmartinezguardiola@gmail.com>
2022-02-01 20:54:14 +0100

    x

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  fetch("https://act3-fullstack-rest-angel.herokuapp.com/players/")
+  fetch("https://act3-fullstack-rest-angel.herokuapp.com/players")
     .then((response) => response.json())
     .then((data) => console.log(data));
 });
