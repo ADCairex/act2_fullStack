253961ecbec365d062482dc04c201b2caaf942bb
ADCairex <angelmartinezguardiola@gmail.com>
2022-02-04 17:38:05 +0100

    x

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
-  let playersContiner = document.getElementById('playersContainer');
-  fetch("https://act3-fullstack-rest-angel.herokuapp.com/players")
+  let playersContiner = document.getElementById("playersContainer");
+  fetch("https://act3-fullstack-rest-angel.herokuapp.com/players", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
     .then((response) => response.json())
     .then((data) => {
       for (i of data) {
